147acdb4b4b4a1d2b129de9bd739b26595a8c830
Rocko Bauman <jackrockocasey@gmail.com>
2024-09-02 10:29:09 -0400

    update .env gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { TopNav } from "./pages/nav/TopNav";
 
 ReactDOM.render(
   <React.StrictMode>

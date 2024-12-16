5731df9565527864a97e2ab59af6c2ba6296965a
Rocko Bauman <jackrockocasey@gmail.com>
2024-12-16 10:08:52 -0800

    add analytics back

--- src/backend/firebase/firebaseConfig.ts
+++ src/backend/firebase/firebaseConfig.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth();

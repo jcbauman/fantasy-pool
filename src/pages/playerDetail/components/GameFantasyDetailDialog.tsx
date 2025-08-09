f5b33df1c182f747264657dbbd145c879a5c66a0
Rocko Bauman <jackrockocasey@gmail.com>
2025-08-09 19:39:33 -0400

    fix fantasy detail modal

--- src/pages/playerDetail/components/GameFantasyDetailDialog.tsx
+++ src/pages/playerDetail/components/GameFantasyDetailDialog.tsx
@@ -1,7 +1,16 @@
-import { Button, Dialog, DialogActions } from "@mui/material";
-import { FC } from "react";
+import { Button, Dialog, DialogActions, Slide } from "@mui/material";
+import { FC, forwardRef, ReactElement, Ref } from "react";
 import { Game, Player } from "../../../types";
 import { GameFantasyDetail } from "./GameFantasyDetail";
+import { TransitionProps } from "@mui/material/transitions";
+
+// Slide transition that slides up
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & { children: ReactElement<any, any> },
+  ref: Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export const GameFantasyDetailDialog: FC<{
   open: boolean;
@@ -11,7 +20,13 @@ export const GameFantasyDetailDialog: FC<{
   scoringMatrix: Record<string, number>;
 }> = ({ open, onClose, game, player, scoringMatrix }) => {
   return (
-    <Dialog open={open} sx={{ width: "100%" }} onClose={onClose}>
+    <Dialog
+      open={open}
+      sx={{ width: "100%" }}
+      onClose={onClose}
+      TransitionComponent={Transition}
+      keepMounted
+    >
       <GameFantasyDetail
         game={game}
         player={player}
